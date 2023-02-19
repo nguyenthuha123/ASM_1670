e175f28710aa817e962654e9358b1ecb4c1064d8
nguyenthuha123 <105527763+nguyenthuha123@users.noreply.github.com>
2023-02-19 12:26:02 +0700

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace _1670_Final.Models
+{
+    public class Category
+    {
+        public List<Book>? books { get; set; }
+        public SelectList? category { get; set; }
+        public string? Bookcate { get; set; }
+    }
+}
